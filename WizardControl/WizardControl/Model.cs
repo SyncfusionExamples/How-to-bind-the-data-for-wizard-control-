bb494de3dd89dcd1b8eb17a6d4cf4733c51fd8e1
Anirudhan Iyyappan <anirudhan.iyyappan@syncfusion.com>
2020-04-06 14:56:08 +0530

    Sample Added

---
+++ WizardControl/WizardControl/Model.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardControl
+{
+    public class Model : NotificationObject
+    {
+        private string title;
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+                RaisePropertyChanged("Title");
+            }
+        }
+
+        private string content;
+        public string Content
+        {
+            get
+            {
+                return content;
+            }
+            set
+            {
+                content = value;
+                RaisePropertyChanged("Description");
+            }
+        }
+    }
+}
---
+++ WizardControl/WizardControl/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardControl
+{
+    public class ViewModel : NotificationObject
+    {
+        private ObservableCollection<Model> pageitems;
+        public ObservableCollection<Model> PageItems
+        {
+            get
+            {
+                return pageitems;
+            }
+            set
+            {
+                pageitems = value;
+                RaisePropertyChanged("PageItems");
+            }
+        }
+
+        public ViewModel()
+        {
+            pageitems = new ObservableCollection<Model>();
+            PopulatePageItems();
+        }
+
+        private void PopulatePageItems()
+        {
+            pageitems.Add(new Model { Title = "Wizard Control", Content = "The WPF Wizard is a tool for building dialogs with multiple pages that guide your end-users through a specific process." });
+            pageitems.Add(new Model { Title = "Pages", Content = "The Wizard control provides pages of three types - start page, regular page and finish (completion) page." });
+            pageitems.Add(new Model { Title = "Button", Content = "Wizard pages can display four buttons - Back, Next, Finish and Cancel. In this article, you will learn how to display the required buttons for each individual page." });
+            pageitems.Add(new Model { Title = "Navigation", Content = "This article demonstrates how the Wizard navigates from one page to another in a simplest scenario, when one page follows another in a strict order." });
+        }
+    }
+}
